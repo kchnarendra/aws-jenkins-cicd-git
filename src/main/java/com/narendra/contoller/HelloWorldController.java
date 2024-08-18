6a204950f10450315320210ed7c176559b87ef1f
kchnarendra <46277254+kchnarendra@users.noreply.github.com>
2024-08-18 06:00:14 +0530

    Update HelloWorldController.java

--- src/main/java/com/narendra/contoller/HelloWorldController.java
+++ src/main/java/com/narendra/contoller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@GetMapping("/test")
 	public String testHelloCICDWorld() {
-		return "Hello World !! Jenkins CICD pipeline on ec2 with ecr repo by Narendra";
+		return "Hello World !! Jenkins CICD pipeline on ec2 with ecr repo by Narendra  V2";
 	}
 }
